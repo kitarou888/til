938ce439f4b7da22bdf5dfc81a7f4f9765edf1cf
きたろう <85793702+kitarou888@users.noreply.github.com>
2024-06-11 21:16:31 +0900

    ブルーベリー本7章力試しその1

--- blueberry_book/src/index.ts
+++ blueberry_book/src/index.ts
@@ -1,78 +1,13 @@
-// type User = {
-//   firstName: string,
-//   lastName: string,
-//   age: number,
-//   favoriteFoods: string[],
-//   hasProgrammingExperience: boolean,
-// }
-// const user: User = {
-//   firstName: '太郎',
-//   lastName: '山田',
-//   age: 24,
-//   favoriteFoods: ['寿司', 'ラーメン', 'カレー'],
-//   hasProgrammingExperience: true,
-// }
+import { readFileSync } from "fs";
 
-// function getSelfIntroduction(user: User): string {
-//   return `私の名前は ${user.lastName} ${user.firstName} です。` +
-//     `年齢は ${user.age} 歳です。` +
-//     `好きな食べ物は ${user.favoriteFoods.join(' と ')} です。` +
-//     `プログラミングの経験${user.hasProgrammingExperience ? 'があります。' : 'はありません。'}`
-// }
+const text: string = readFileSync("./uhyo.txt", "utf8");
 
-// console.log(getSelfIntroduction(user))
+let position = text.indexOf("uhyo");
+let count = 0;
 
-// type User = { name: string };
-// function getUserName(maybeUser: unknown): string | undefined {
-//   if (typeof maybeUser !== 'object' || maybeUser === null) {
-//     return undefined
-//   }
+while (position !== -1) {
+  count++;
+  position = text.indexOf("uhyo", position + 1);
+}
 
-//   const user = maybeUser as Record<keyof User, unknown>;
-
-//   if (typeof user.name !== 'string') {
-//     return undefined
-//   }
-//   return user.name
-// }
-
-// type Subject =
-//   | { type: "name"; payload: string }
-//   | { type: "favorite_food"; payload: string[] };
-
-// function printSelfIntroduction(subject: Subject) {
-//   switch (subject.type) {
-//     case "name":
-//       console.log(`私の名前は${subject.payload}です`);
-//       return;
-//     case "favorite_food":
-//       console.log(`私の好きな食べ物は${subject.payload.join("と")}です`);
-//       return;
-//     default:
-//       throw Error('ありません！')
-//   }
-// }
-
-// function triple<T>(arg: T): T[] {
-//   return [arg, arg, arg]
-// }
-
-// console.log(triple(123))
-
-type Animal = { name: string };
-type Dog = { name: string; forefoot: "前足" };
-const dog: Dog = { name: "イヌ", forefoot: "前足" };
-
-let dogToAnimal = (dog: Dog): Animal => dog;
-// より部分な型は代入できる→返り値は部分が、引数はスーパーが入る
-dogToAnimal = (dog: Dog): Dog => {
-  return dog;
-};
-
-const animal: Animal = dogToAnimal(dog);
-
-type D = { prop: { a: number } } & { prop: { b: string } };
-type E = number & 12345;
-type F = "love" & "peace";
-
-// type Multiply = <a, b>(a * b)
+console.log(count);
