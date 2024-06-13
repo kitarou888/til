03541450396ca9e4147576459644495366281eea
きたろう <85793702+kitarou888@users.noreply.github.com>
2024-06-13 20:57:06 +0900

    blueberry 7.3力試し

--- blueberry_book/src/index.ts
+++ blueberry_book/src/index.ts
@@ -1,6 +1,13 @@
 import { readFileSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const text: string = readFileSync("./uhyo.txt", "utf8");
+const execDir = path.parse(import.meta.url).dir;
+// console.log(path.join(fileURLToPath(execDir), "../uhyo.txt"));
+const filePath = path.join(fileURLToPath(execDir), "../uhyo.txt");
+
+const text: string = readFileSync(filePath, "utf8");
+// const text: string = readFileSync("uhyo.txt", "utf8"); // 実行場所によってファイルパスを変えないといけない
 
 let position = text.indexOf("uhyo");
 let count = 0;
