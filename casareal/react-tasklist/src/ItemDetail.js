89a8c1188d29ee6f32ae639b6cbd7efa357e2503
kitarou888 <85793702+kitarou888@users.noreply.github.com>
2024-06-22 09:22:34 +0900

    脆弱性対応

--- casareal/react-tasklist/src/ItemDetail.js
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-
-const ItemDetail = () => {
-  const { pageno } = useParams();
-  return <div>ページNo. {pageno}</div>;
-};
-
-export default ItemDetail;
